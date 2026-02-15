73fe6881ffb6a834a50e28d66a186fa615f49f3c
Yana <anaermakova948@gmail.com>
2026-02-15 12:14:51 +0800

    Add C++ exam tasks

---
+++ экзамен/новое.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int n;                      // сколько точек
+double ras[10][10];         // расстояния
+int best[10];               // лучшее паросочетание
+double min_sum = 1e9;       // минимальная сумма
+
+// перебирает все варианты
+void perebor(int p[], int k, bool used[]) {
+    if (k == n) { // если все красные разобрали
+        double s = 0;
+        for (int i = 0; i < n; i++) {
+            s += ras[i][p[i]]; // сумма расстояний
+        }
+        if (s < min_sum) { // если нашли лучше
+            min_sum = s;
+            for (int i = 0; i < n; i++) {
+                best[i] = p[i]; // запомнили лучшее
+            }
+        }
+        return;
+    }
+
+    // пробуем все синие для текущей красной
+    for (int i = 0; i < n; i++) {
+        if (!used[i]) { // если синяя свободна
+            used[i] = true; // заняли
+            p[k] = i;       // соединили
+            perebor(p, k + 1, used); // дальше рекурсия
+            used[i] = false; // освободили (назад)
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian"); 
+
+    cout << "Введите количество точек: ";
+    cin >> n;
+    double kx[10], ky[10]; // красные x,y
+    double sx[10], sy[10]; // синие x,y
+    // генерим точки
+    cout << "\nКрасные:\n";
+    for (int i = 0; i < n; i++) {
+        kx[i] = rand() % 60;
+        ky[i] = rand() % 60;
+        cout << i + 1 << ": " << kx[i] << " " << ky[i] << endl;
+    }
+
+    cout << "\nСиние:\n";
+    for (int i = 0; i < n; i++) {
+        sx[i] = rand() % 60;
+        sy[i] = rand() % 60;
+        cout << i + 1 << ": " << sx[i] << " " << sy[i] << endl;
+    }
+
+    // считаем расстояния
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            double dx = kx[i] - sx[j];
+            double dy = ky[i] - sy[j];
+            ras[i][j] = sqrt(dx * dx + dy * dy);
+        }
+    }
+
+    // перебор
+    int p[10];
+    bool used[10] = { false };
+    perebor(p, 0, used);
+
+    // результат
+    cout << "\nЛучший вариант:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "К" << i + 1 << " -> С" << best[i] + 1
+            << " = " << ras[i][best[i]] << endl;
+    }
+    cout << "Сумма: " << min_sum << endl;
+
+    return 0;
+}
---
+++ экзамен/экзамен.cpp
@@ -0,0 +1,128 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+const int N = 200;
+double dist[N][N], u[N], v[N], minv[N];
+int p[N], way[N];
+bool used[N];
+int n;
+
+bool dfs(int i) {
+    p[0] = i;
+    int j0 = 0;
+
+    for (int j = 0; j <= n; j++) {
+        minv[j] = 1e18;
+        used[j] = false;
+    }
+
+    do {
+        used[j0] = true;
+        int i0 = p[j0];
+        double delta = 1e18;
+        int j1 = 0;
+
+        for (int j = 1; j <= n; j++) {
+            if (!used[j]) {
+                double cur = dist[i0][j] - u[i0] - v[j];
+                if (cur < minv[j]) {
+                    minv[j] = cur;
+                    way[j] = j0;
+                }
+                if (minv[j] < delta) {
+                    delta = minv[j];
+                    j1 = j;
+                }
+            }
+        }
+
+        for (int j = 0; j <= n; j++) {
+            if (used[j]) {
+                u[p[j]] += delta;
+                v[j] -= delta;
+            }
+            else {
+                minv[j] -= delta;
+            }
+        }
+
+        j0 = j1;
+    } while (p[j0] != 0);
+
+    do {
+        int j1 = way[j0];
+        p[j0] = p[j1];
+        j0 = j1;
+    } while (j0 != 0);
+
+    return true;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Введите n: ";
+    cin >> n;
+
+    if (n <= 0 || n >= N) {
+        cout << "Ошибка: n должно быть от 1 до " << N - 1 << endl;
+        return 1;
+    }
+
+    srand(time(0));
+
+    double red_x[N], red_y[N], blue_x[N], blue_y[N];
+
+    // Генерация точек
+    cout << "\nКрасные точки:\n";
+    for (int i = 1; i <= n; i++) {
+        red_x[i] = rand() % 100;
+        red_y[i] = rand() % 100;
+        cout << i << ": " << red_x[i] << " " << red_y[i] << endl;
+    }
+
+    cout << "\nСиние точки:\n";
+    for (int i = 1; i <= n; i++) {
+        blue_x[i] = rand() % 100;
+        blue_y[i] = rand() % 100;
+        cout << i << ": " << blue_x[i] << " " << blue_y[i] << endl;
+    }
+
+    // Матрица расстояний
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            double dx = red_x[i] - blue_x[j];
+            double dy = red_y[i] - blue_y[j];
+            dist[i][j] = sqrt(dx * dx + dy * dy);
+        }
+    }
+
+    // Инициализация
+    for (int i = 0; i <= n; i++) {
+        u[i] = v[i] = 0;
+        p[i] = 0;
+    }
+
+    // Алгоритм
+    for (int i = 1; i <= n; i++) {
+        dfs(i);
+    }
+
+    // Результат
+    double sum = 0;
+    cout << "\nПаросочетание:\n";
+    for (int j = 1; j <= n; j++) {
+        sum += dist[p[j]][j];
+        cout << "R" << p[j] << " → B" << j << ": "
+            << fixed << setprecision(3) << dist[p[j]][j] << endl;
+    }
+
+    cout << fixed << setprecision(3);
+    cout << "\nСумма: " << sum << endl;
+
+    return 0;
+}
